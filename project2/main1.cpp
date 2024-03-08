6986e5a23caee7520cdee16ed97536e1e2cceed3
src_resources <src.res.211@gmail.com>
2024-03-08 21:26:31 +0800

    Update

--- project2/main1.cpp
+++ project2/main1.cpp
@@ -112,6 +112,19 @@ float vertices2[] = {
         -0.5f,  0.5f, -0.5f
 };
 
+glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
 static GLchar infoLog[10240];
 
 float cameraSpeed = 0.2f;
@@ -239,12 +252,12 @@ int main()
         glm::mat4 projection(1.0f); // convert to clip space
         projection = glm::perspective(glm::radians(45.0f), (float) screenWidth / (float) screenHeight, 0.1f, 100.0f);
 
-        {
+        for (unsigned int i = 0; i < 10; i++) {
             ourShader1.use();
 
             ourShader1.setVec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
             glm::mat4 model(1.0f); // convert to world space
-            model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+            model = glm::translate(model, cubePositions[i]);
             ourShader1.setMat4("model", model);
             ourShader1.setMat4("view", view);
             ourShader1.setMat4("projection", projection);
